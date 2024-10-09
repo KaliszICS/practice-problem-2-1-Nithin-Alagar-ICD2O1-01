92e3880a5cc6bb882736f003cc62367f20b928ce
Nithin-Alagar-ICD2O1-01 <nithin.alagar@student.tdsb.on.ca>
2024-10-09 13:44:53 +0000

    nithin

--- PP2_1.py
+++ PP2_1.py
@@ -2,15 +2,24 @@
 
 def q1(): 
   #Write Assignment code here
-
-
+  num = int(input("In: "))
+  if num %2 == 0:
+    print(f"{num} is even")
+  if num %2 == 0:
+    print(f"{num} is odd")
 def q2(): 
   #Write Assignment code here
+  name = input("In: ")
+  if name == "kalisz":
+    print("teacher")
+  if name != "kalisz":
+    print("student")
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
-
+'''
 q1()
 q2()
+'''
